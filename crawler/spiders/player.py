ae350a61624e0b86fab49972c2d0b0b77bfbaa60
ownweight1 <kanamorishuhei@gmail.com>
2020-02-08 01:14:11 +0900

    fix player spider

--- crawler/spiders/player.py
+++ crawler/spiders/player.py
@@ -15,40 +15,39 @@ class PlayerSpider(scrapy.Spider):
         yield scrapy.Request(self.start_urls[0], self.crawl_npb_url)
 
     def crawl_npb_url(self, response):
-        TEAMS_URLS = response.xpath("//*[@id='team_list']/div/ul/li/a/@href")
+        TEAM_URLS = response.xpath("//*[@id='team_list']/div/ul/li/a/@href")
 
-        for n, TEAM_URL in enumerate(TEAMS_URLS):
+        for n, TEAM_URL in enumerate(TEAM_URLS):
             if utils.is_development() and n > 0: return
 
             URL = response.urljoin(TEAM_URL.extract())
             yield scrapy.Request(URL, self.crawl_team_url)
 
     def crawl_team_url(self, response):
-        PLAYERS_URLS = response.xpath("//*[@class='rosterRegister']/a/@href")
+        PLAYER_URLS = response.xpath("//*[@class='rosterRegister']/a/@href")
 
-        for n, PLAYER_URL in enumerate(PLAYERS_URLS):
+        for n, PLAYER_URL in enumerate(PLAYER_URLS):
             if utils.is_development() and n > 0: return
 
             URL = response.urljoin(PLAYER_URL.extract())
-            yield scrapy.Request(URL, self.parse_player_item)
+            yield scrapy.Request(URL, self.crawl_player_url)
 
+    def crawl_player_url(self, response):
 
-    def parse_player_item(self, response):
-
-        def player_item(player, str):
-            with open('xpath/player_xpath.json', 'r') as f:
-                player[str] = response.xpath(json.load(f)[str]).extract()
+        def parse_player_item(player, s):
+            with open('crawler/xpath/player_xpath.json', 'r') as f:
+                text = ''.join(response.xpath(json.load(f)[s]).extract())
+                player[s] = utils.stlip_space_crlf(text)
 
         player = Player()
-        player_item(player, 'no')
-        player_item(player, 'team')
-        player_item(player, 'name')
-        player_item(player, 'kana')
-        player_item(player, 'position')
-        player_item(player, 'bat_and_throw')
-        player_item(player, 'height_and_weight')
-        player_item(player, 'birth')
-        player_item(player, 'carrer')
-        player_item(player, 'draft')
+        parse_player_item(player, 'no')
+        parse_player_item(player, 'team')
+        parse_player_item(player, 'name')
+        parse_player_item(player, 'kana')
+        parse_player_item(player, 'position')
+        parse_player_item(player, 'bat_and_throw')
+        parse_player_item(player, 'height_and_weight')
+        parse_player_item(player, 'birth')
+        parse_player_item(player, 'carrer')
+        parse_player_item(player, 'draft')
 
-        print(player)
--- crawler/utils.py
+++ crawler/utils.py
@@ -3,3 +3,6 @@ import os
 def is_development():
     return os.environ['ENV'] == 'development'
 
+def stlip_space_crlf(s):
+    dic = str.maketrans({' ': '', '\n': '', '\r': '', '\u3000': ' ', '・': ' '})
+    return s.translate(dic)
