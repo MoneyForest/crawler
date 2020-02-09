806dc85df9fddcb9a545aa93d9e95e83849cfea1
ownweight1 <kanamorishuhei@gmail.com>
2020-02-09 21:44:28 +0900

    add reshape player method

--- crawler/utils.py
+++ crawler/utils.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 
 def is_development():
@@ -6,3 +7,24 @@ def is_development():
 def stlip_space_crlf(s):
     dic = str.maketrans({' ': '', '\n': '', '\r': '', '\u3000': ' ', '・': ' '})
     return s.translate(dic)
+
+def reshape_npb_id(s):
+    return s.split('_')[2].split('.')[0]
+
+def reshape_height(s):
+    return s.split('／')[0]
+
+def reshape_weight(s):
+    return s.split('／')[1]
+
+def reshape_throw(s):
+    return s.split('投')[0]
+
+def reshape_bat(s):
+    return s.split('投')[1].split('打')[0]
+
+def reshape_draft_year(s):
+    return s.split('年')[0]
+
+def reshape_draft_no(s):
+    return s.split('ドラフト')[1].split('位')[0]
