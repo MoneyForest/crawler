865726b06dad4699acd047eb1e19667bd204be54
ownweight1 <kanamorishuhei@gmail.com>
2020-02-16 21:19:59 +0900

    refact method

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-import boto3
+from . import player_utils
 
 
 class CrawlerPipeline(object):
 
     def process_item(self, item, spider):
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('player')
-        if 'Item' not in table.get_item(Key={'npb_id': item['npb_id'], 'team_en': item['team_en']}):
+        table = player_utils.init_player_table()
+        item = player_utils.replace_none_to_space(item)
+        key = player_utils.table_keys(item)
+
+        if 'Item' not in table.get_item(Key=key):
             table.put_item(Item=dict(item))
         return
